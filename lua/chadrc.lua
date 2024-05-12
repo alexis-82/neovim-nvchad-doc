135b90442a61a375a659ccedd68ef28854091af9
alexis-82 <giobbia82@gmail.com>
2024-05-12 23:13:10 +0200

    new feature

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,4 +1,4 @@
--- This file  needs to have same structure as nvconfig.lua
+-- This file  needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
@@ -7,10 +7,9 @@ local M = {}
 M.ui = {
 	theme = "ayu_dark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+	Comment = { fg = "dark_purple", italic = true },
+	["@comment"] = { fg = "dark_purple", italic = true },
+	},
 }
-
 return M
